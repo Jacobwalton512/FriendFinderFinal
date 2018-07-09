f51e2bb2e9ee8049db7b50b394a78a2c226a32de
Jacob Walton <JacobWalton@Jacobs-MacBook-Pro.local>
2018-07-09 16:36:03 -0500

    update

--- data/friendData.js
+++ data/friendData.js
@@ -1,9 +1,3 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
 var friendArray = [
   {
     name: "Tiffany",
@@ -12,6 +6,4 @@ var friendArray = [
   }
 ];
 
-
-// Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendArray;
